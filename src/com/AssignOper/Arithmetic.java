6a05b82a01ed1a0502dcc1e5e1ad363b71abab55
Administrator <Administrator@172.16.51.8>
2024-08-12 21:52:58 -0700

    Commit for Assignment and operator

---
+++ src/com/AssignOper/Arithmetic.java
@@ -0,0 +1,25 @@
+package com.AssignOper;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int result = x + y;
+		System.out.println("x + y = " + result);
+		result = x-y;
+		
+		System.out.println("x - y = " + result);
+		
+		result = x*y;
+		System.out.println("x * y = " + result);
+		
+		result = y/x;
+		System.out.println("x / y = " + result);
+		
+		result = x % 3;
+		System.out.println("x % 3" + result);
+
+	}
+
+}
